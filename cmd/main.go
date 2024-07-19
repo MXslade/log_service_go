5cb495340539ea54b354cce9b69cf68457b54a90
mxslade <turar0408@gmail.com>
2024-07-19 07:55:21 +0500

    feat: godotenv

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/MXslade/log_service_go/config"
 	"github.com/MXslade/log_service_go/route"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags)
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	log.Println("Initializing echo")
 	e := echo.New()
 
 	route.SetUpRoutes(e)
 
-	e.Logger.Fatal(e.Start(":1313"))
+	appPort, ok := os.LookupEnv("APP_PORT")
+	if ok {
+		e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", appPort)))
+	} else {
+		log.Printf("no APP_PORT env value is set, using default: %v\n", config.DefaultAppPort)
+		e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", config.DefaultAppPort)))
+	}
 }
---
+++ config/config.go
@@ -0,0 +1,3 @@
+package config
+
+const DefaultAppPort = "1313"
